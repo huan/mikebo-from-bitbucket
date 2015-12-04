c97257a4ebf1b91d6f7bc597f740e2cb6c7f0422
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2015-12-04 13:40:06 +0800

    change pull down directory

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -31,8 +31,9 @@ GAPPS_CMD="$BASE_DIR/../node_modules/.bin/gapps"
 }
 
 fileId=$(grep fileId "$GAPPS_CONFIG" | cut -d'"' -f4)
-[ -n "$fileId" ] || {
-  echo "fileId not found!"
+path=$(grep path "$GAPPS_CONFIG" | cut -d'"' -f4)
+[ -n "$fileId" && -n "$path"] || {
+  echo "fileId or path not found!"
   exit 255
 }
 
@@ -41,7 +42,7 @@ echo -n "Start pulling GAS script id: $fileId from ggoogle drive... "
 mv -f "$GAPPS_CONFIG" "${GAPPS_CONFIG}.bak"
 
 cd "$BASE_DIR/.."
-$GAPPS_CMD clone $fileId
+$GAPPS_CMD clone $fileId $path
 
 echo "Done."
 echo
