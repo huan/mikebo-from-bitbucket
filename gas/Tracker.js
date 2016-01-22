9768d54cea37f79708ec915cc176a18cdc62ed3c
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-01-22 20:15:50 +0800

    add Tracker module

---
+++ gas/Tracker.js
@@ -0,0 +1,53 @@
+var Tracker = (function () {
+  'use strict'
+  
+  var VERSION = '0.1.0'
+  
+  var Tracker = function () {
+  }
+  
+  Tracker.logOnEnd = logOnEnd
+  Tracker.logOnStart = logOnStart
+    
+  return Tracker
+  
+  
+  ///////////////////////////////////////////////////////////
+  
+  
+  function logOnStart(req, res, next) {
+    req.startTime = new Date()
+    req.errors = []
+    log(log.DEBUG, '%s start processing %s'
+        , req.getChannelName ? req.getChannelName() : 'unknown'
+        , req.getThread      ? req.getThread().getFirstMessageSubject() : 'unknown'
+       )
+    next()
+  }
+  
+  function logOnEnd(req, res, next) {
+    var errorMsg
+    
+    if (req.errors.length) {
+      errorMsg = req.errors.map(function (e) { 
+        if (e instanceof Error) {
+          return e.name + ':' + e.message + ':' + e.stack
+        } else {
+          return e
+        }
+      }).join(',')
+    } else { 
+      errorMsg = ''
+    }
+    
+    log(log.NOTICE, 'C(%s/%ss)[%s] %s'
+        , req.getChannelName ? req.getChannelName() : 'unknown'
+        , Math.floor((new Date() - req.startTime)/1000)
+        
+        , req.getThread ? req.getThread().getFirstMessageSubject() : 'unknown'
+        , errorMsg
+       )
+    next()
+  }
+  
+}())
