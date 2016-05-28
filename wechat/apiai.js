4b173529e49c96e1b7433e05255deb622d5dfe0c
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-05-28 17:18:25 +0000

    wechat robot powered by api.ai

---
+++ wechat/apiai.js
@@ -0,0 +1,155 @@
+/**
+ *
+ * Wechaty bot use a ApiAi.com brain
+ *
+ * Apply Your Own ApiAi Developer API_KEY at: 
+ * http://www.api.ai
+ *
+ * Enjoy!
+ *
+ * Wechaty - https://github.com/zixia/wechaty
+ *
+ */
+const log = require('npmlog')
+const co  = require('co')
+const EventEmitter2 = require('eventemitter2')
+
+const Wechaty = require('wechaty')
+//log.level = 'verbose'
+// log.level = 'silly'
+
+const APIAI_API_KEY = 'b8784435932c48bab2e34d0aa3c2d365'
+const ApiAi = require('apiai')
+const apiAi = new ApiAi(APIAI_API_KEY)
+
+const bot = new Wechaty({head: false})
+
+console.log(`
+Mike@Wechat Loading...
+`)
+
+bot
+.on('scan', ({url, code}) => {
+  console.log(`[${code}]Scan qrcode in url to login:\n${url}`)
+})
+.on('login'  , user => log.info('Bot', `bot login: ${user}`))
+.on('logout' , e => log.info('Bot', 'bot logout.'))
+.on('message', m => {
+  co(function* () {
+    yield m.ready()
+
+    log.info('Bot', 'recv: %s'  , m)
+    if (m.group()) {  // group message
+     if (/Wechaty/i.test(m.group().name())) {
+      log.info('Bot', 'wechaty talk: %s'  , m)
+      talk(m)
+     }
+    } else {          // not group message
+      if (m.from().stranger()) {
+        log.info('Bot', 'stranger talk: %s'  , m)
+        talk(m)
+      }
+    }
+  })
+  .catch(e => log.error('Bot', 'on message rejected: %s' , e))
+})
+
+bot.init()
+.catch(e => {
+  log.error('Bot', 'init() fail:' + e)
+  bot.quit()
+  process.exit(-1)
+})
+
+class Talker extends EventEmitter2 {
+  constructor(thinker) {
+    log.verbose('Talker()')
+    super()
+    this.thinker = thinker
+    this.obj = {
+      text: []
+      , time: []
+    }
+    this.timer = null
+  }
+
+  save(text) {
+    log.verbose('Talker', 'save(%s)', text)
+    this.obj.text.push(text)
+    this.obj.time.push(Date.now())
+  }
+  load() {
+    const text = this.obj.text.join(', ')
+    log.verbose('Talker', 'load(%s)', text)
+    this.obj.text = []
+    this.obj.time = []
+    return text
+  }
+  
+  updateTimer(delayTime) {
+    delayTime = delayTime || this.delayTime()
+    log.verbose('Talker', 'updateTimer(%s)', delayTime)
+    
+    if (this.timer) { clearTimeout(this.timer) }
+    this.timer = setTimeout(this.say.bind(this), delayTime)
+  }
+  
+  hear(text) {
+    log.verbose('Talker', `hear(${text})`)
+    this.save(text)
+    this.updateTimer()
+  }
+  say() {
+    log.verbose('Talker', 'say()')
+    const text  = this.load()
+    this.thinker(text)
+    .then(reply => this.emit('say', reply))
+    this.timer = null
+  }
+  
+  delayTime() {
+    const minDelayTime = 100
+    const maxDelayTime = 500
+    const delayTime = Math.floor(Math.random() * (maxDelayTime - minDelayTime)) + minDelayTime
+    return delayTime
+  }
+}
+
+var Talkers = []
+
+function talk(m) {
+  const fromId  = m.from().id
+  const groupId = m.group().id
+  const content = m.content().replace(/(<([^>]+)>)/ig,'')
+  
+  let talkerName  = fromId + groupId
+  talkerName      = require('crypto').createHash('md5').update(talkerName).digest("hex");
+
+  if (!Talkers[talkerName]) {
+    Talkers[talkerName] = new Talker(function(text) {
+      return new Promise((resolve, reject) => {
+        apiAi.textRequest(text, {
+          language:     'zh-cn'
+          , sessionId:  talkerName
+        })
+        .on('response', function(response) {
+          console.log(response)
+          const reply = response.result.fulfillment.speech
+          if (!reply) {
+            log.info('ApiAi', `Talker[${talkerName}] do not want to talk for "${text}"`)
+            return reject()
+          }
+          log.info('ApiAi', `Talker[%s] reply:"%s" for "%s" `, talkerName, reply, text)
+          return resolve(reply)
+        })
+        .on('error', function(error) {
+          log.error('ApiAi', error)
+          reject(error)
+        })
+        .end()
+      })
+    })
+    Talkers[talkerName].on('say', reply => bot.reply(m, reply))
+  }
+  Talkers[talkerName].hear(content)
+}
